aa519dce391733566997e894eb5c4e568fbb6f16
RICKCOYL <rickoburu@gmail.com>
2020-11-26 10:57:20 +0300

    add events to show on index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:create, :new, :show]
-  resources :events, only: [:create, :new, :show]
+
+  resources :events, only: [:create, :new, :show, :index]
 end

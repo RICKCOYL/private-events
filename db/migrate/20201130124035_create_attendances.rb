3a6109d2b0d946f8e0aa7e62d14533fddf7bc731
Lidya1234 <lidya94gher@gmail.com>
2020-11-30 18:55:46 +0300

    Fix navbar

--- db/migrate/20201130124035_create_attendances.rb
+++ db/migrate/20201130124035_create_attendances.rb
@@ -1,10 +1,12 @@
 class CreateAttendances < ActiveRecord::Migration[6.0]
   def change
     create_table :attendances do |t|
-      t.references :attendee, null: false
-      t.references :attended_events, null: false
+      t.references :attendee, null: false, references: :users, index: true
+      t.references :attended_events, null: false, references: :events, index: true
 
       t.timestamps
     end
+    add_foreign_key :attendances, :users, column: :attendee_id
+    add_foreign_key :attendances, :events, column: :attended_event_id
   end
 end

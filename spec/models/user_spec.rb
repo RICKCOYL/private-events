da9adf576b6455e61402c30876f5c63dbebfbde5
RICKCOYL <rickoburu@gmail.com>
2020-12-01 17:19:28 +0300

    add validation testing for username

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,15 +2,20 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   before :each do
-    @user = User.create(name: 'herry')
+    @user = User.create(username: "simon")
     @event1 = @user.created_events.create(date: '02.09.2030', description: 'Party')
     @event2 = @user.created_events.create(date: '02.09.2020', description: 'Pijamas')
    
   end
 
   
-  context 'has associations' do
+  context 'has associations and validations' do
     it 'can be created' do
       expect(User.first).to eq(@user)
     end
+
+    it 'username should be present' do
+      assert @user.valid?
+    end
+  end
 end

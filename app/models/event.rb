90b5a7eb9367092b1ec0122c1590011cb0b0fbc0
Lidya1234 <lidya94gher@gmail.com>
2020-12-01 15:12:08 +0300

    Add rspec files

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
     belongs_to :creator, class_name: "User"
     has_many :attendances, foreign_key: 'attended_events', class_name: 'Attendance'
-    has_many :attendees, through: :attendances, source: 'attendee'
+    has_many :attendees, :through => :attendances, :source => :attendee
 
     scope :upcoming_events, -> { where('date >= ?', Date.today) }
     scope :past_events, -> { where('date < ?', Date.today) }
